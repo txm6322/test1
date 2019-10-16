4a4134e29501d15a1a0c44da0ed81460741ee1a0
tma91 <tma91@TieyiMacPro.fios-router.home>
2019-10-15 23:08:54 -0400

    mod

--- cpp/enum.cpp
+++ cpp/enum.cpp
@@ -1,5 +1,14 @@
 // for backward compatible with C, which does not have namespace, so enum has to be in the
-// global namespace.
+// 1. global namespace.
+enum B{a,b,c}; // a=0, b=1, c=2
+enum SameValue{d=0,e,f=0); // can have same value for d,f, e = d+1
+// 2. implicitly convert to int types
+int k = a;
+// 3. c++98: default underlying type is not defined
+// 4. C++11: can forward declaration of enum, if specified a fixed underlying type
+enum {ER=0, E1} va1; // unnamed enum
+enum C: char {a1,b1}; // cannot define a
+enum E: short int {k1=0x01, k3=0x100};
 enum Days {Mon,Tue}; // unscoped. Mon, Tue is leaked/:defined in global scope, cannot be redefined
 Days day = Mon; // C++ way
 enum Days day1; // C way, requires enum
